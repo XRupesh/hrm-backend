0bcb11a659304638c84b30f5e189a3d6bfeec386
Rupesh <rupeshgaudel3@gmail.com>
2023-05-08 16:14:59 +0545

    dummy controller, service created

--- src/main/java/com/hrmtool/service/EmployeeService.java
+++ src/main/java/com/hrmtool/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.hrmtool.service;
 
+import com.hrmtool.persistance.dto.EmployeeDto;
+
 public interface EmployeeService {
+    EmployeeDto getEmployeeById(Integer id);
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto updateEmployee(EmployeeDto employeeDto);
+
+    void deleteEmployeeById(Integer id);
+
 }
--- src/main/java/com/hrmtool/service/serviceImplementation/EmployeeServiceImpl.java
+++ src/main/java/com/hrmtool/service/serviceImplementation/EmployeeServiceImpl.java
@@ -1,13 +1,41 @@
 package com.hrmtool.service.serviceImplementation;
 
+import com.hrmtool.persistance.dto.EmployeeDto;
+import com.hrmtool.persistance.entity.Employee;
+import com.hrmtool.persistance.repository.EmployeeRepo;
+import com.hrmtool.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @Transactional
 @Slf4j
 @AllArgsConstructor
-public class EmployeeServiceImpl {
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepo employeeRepository;
+
+
+    @Override
+    public EmployeeDto getEmployeeById(Integer id) {
+       return null;
+    }
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployeeById(Integer id) {
+    }
 }
