aaa72e2f04cc8059da30da6fe80bca5ca7b8a2b8
Rupesh <rupeshgaudel3@gmail.com>
2023-05-09 15:49:33 +0545

    conflict resolved

--- src/main/java/com/hrmtool/persistance/repository/JobRepo.java
+++ src/main/java/com/hrmtool/persistance/repository/JobRepo.java
@@ -1,9 +1,9 @@
 package com.hrmtool.persistance.repository;
 
-import com.hrmtool.persistance.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface JobRepo extends JpaRepository<Job,Integer> {
+import com.hrmtool.persistance.entity.Job;
+
+public interface JobRepo extends JpaRepository<Job, Integer>{
+
 }
